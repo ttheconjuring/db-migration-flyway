f27a5972570a9f5b1e283dc018b95270d1051d6c
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-06 19:06:57 +0200

    Book entity and repository created

---
+++ src/main/java/com/example/demo/entities/Book.java
@@ -0,0 +1,26 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    @Basic
+    private String title;
+
+}
