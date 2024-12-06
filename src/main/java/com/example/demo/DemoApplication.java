cff55cfea57add590fe38fea9887dbc0846d7bb6
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-06 22:04:31 +0200

    small changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,26 +1,24 @@
 package com.example.demo;
 
-import com.example.demo.repositories.BookRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-@Log4j2
-public class DemoApplication implements CommandLineRunner {
+// @RequiredArgsConstructor
+// @Log4j2
+public class DemoApplication {
 
-    private final BookRepository bookRepository;
+    // private final BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Override
+    /* @Override
     public void run(String... args) {
         log.info("Data in table 'books': ");
         this.bookRepository.findAll().forEach(book -> System.out.println(book));
-    }
+    } */
+
+    // TODO: you can remove the comments after the the install command has finished successfullyclear
 }
