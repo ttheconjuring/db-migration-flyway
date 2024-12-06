dda0829442f72c33087fa479dcbbf0a8c644861d
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-06 22:17:50 +0200

    redundant code deleted

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,21 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// @RequiredArgsConstructor
-// @Log4j2
 public class DemoApplication {
-
-    // private final BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    /* @Override
-    public void run(String... args) {
-        log.info("Data in table 'books': ");
-        this.bookRepository.findAll().forEach(book -> System.out.println(book));
-    } */
-
-    // TODO: you can remove the comments after the the install command has finished successfullyclear
 }
