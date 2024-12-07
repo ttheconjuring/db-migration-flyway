88110289e1b42cd493bf54149bb668ba51a28108
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-07 10:34:20 +0200

    ?createDatabaseIfNotExist=true added

---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @Basic
+    private String title;
+
+    @Basic
+    private String category;
+
+}
