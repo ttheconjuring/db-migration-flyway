6f183fb4649f679c1445a39fee9ce7d2b34e1401
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-11 18:28:11 +0200

    over

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@RequiredArgsConstructor
+public class DemoApplication implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        this.bookRepository.findAll().forEach(System.out::println);
+    }
 }
